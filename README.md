a470104e29c5da4105d59526784c3e0a1674bce0
rhgksrua <rhgksrua@gmail.com>
2016-06-29 02:26:40 +0000

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # rios
 Ticket System
+
+## Development
+
+Clone this repository
+
+Run `npm install`.
+
+Run `node server.js` or `npm start`.
+
+
+## Notes
+
+* Running on node version v4.4.5
---
+++ server.js
@@ -0,0 +1,38 @@
+'use strict';
+
+var express = require('express');
+var morgan = require('morgan');
+var bodyParser = require('body-parser');
+
+// Setting environment variables through .env file
+require('dotenv').config({silent: true});
+
+var app = express();
+
+// Logging
+app.use(morgan('combined'));
+
+// Parse forms/json
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+var port = process.env.PORT || 3000;
+
+// Tempaltes
+app.set('view engine', 'pug');
+app.set('views', './templates');
+
+// public directory.
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', index);
+
+function index(req, res) {
+    return res.render('index');
+}
+
+app.listen(port, function() {
+    console.log('http://localhost:' + port);
+});
